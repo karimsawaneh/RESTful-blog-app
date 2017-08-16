2565c51febf8f616585c560a6d3bab15eba928ce
Karim <sakjrn@gmail.com>
2017-08-17 01:17:20 +0200

    The app is pretty much done. All that's left now is adding some user authentication and maybe comments. But this was basically to practice RESTful routing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    methodOverride = require('method-override'),
+    expressSanitizer = require('express-sanitizer');
     app = express();
 
     // setup mongoose
@@ -12,6 +14,8 @@ mongoose.connect('mongodb://localhost/restful_blog_app', {
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride('_method'));
 
 // mongoose/model config
 var blogSchema = new mongoose.Schema({
@@ -47,6 +51,7 @@ app.get('/blogs/new', function(req, res){
 // Create Post route
 app.post('/blogs', function(req, res){
     //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body); //sanitizes the input
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render('new');
@@ -75,6 +80,31 @@ app.get('/blogs/:id/edit', function(req, res){
             res.redirect('/blogs');
         } else {
             res.render('edit', {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE Route
+app.put('/blogs/:id', function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body); //sanitizes the input
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/' + req.params.id);
+        }
+    });
+});
+
+// DELETE Route
+app.delete('/blogs/:id', function(req, res){
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            //redirect somewhere
+            res.redirect('/blogs');
         }
     });
 });
